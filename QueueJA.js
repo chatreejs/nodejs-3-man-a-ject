ecd5054d13b6c2ac871752b0da5b5a4a9482e706
kornranat vorawatjirapak <swu59238@outlook.com>
2019-03-25 17:05:43 +0700

    Add QueueJA.js

---
+++ QueueJA.js
@@ -0,0 +1,31 @@
+class QueueJA {
+    enqueue(e) {
+
+    }
+
+    dequeue() {
+
+    }
+
+    front() {
+
+    }
+
+    isEmpty() {
+
+    }
+
+    isFull() {
+
+    }
+
+    clear() {
+
+    }
+
+    size() {
+        
+    }
+}
+
+module.exports = QueueJA;
