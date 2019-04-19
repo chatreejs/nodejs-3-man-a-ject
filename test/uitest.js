0c92467d36e1e8e0ec846bcfa0b4f2596c7cd996
Pongsapon A-nong <swu59266@outlook.com>
2019-04-19 14:59:30 +0700

    add name region

--- test/uitest.js
+++ test/uitest.js
@@ -22,12 +22,29 @@ describe('Checkout Homepage', function () {
         // Find the north button
         await driver.findElement(By.name('north')).click();
         // Find the Chiang Rai button
-        await driver.findElement(By.name('cnx')).click();
+        await driver.findElement(By.name('chiang_rai')).click();
 
         let city = await driver.findElement(By.name('city')).getText();
         // console.log(city);
         assert.equal(city, 'Chiang Rai');
     });
+
+    // it('Click on Chiang Rai', async function () {
+    //     // Set browser size to maximize
+    //     await driver.manage().window().maximize()
+    //     // Load the page
+    //     await driver.get('http://localhost:3000');
+    //     // Find the province button
+    //     await driver.findElement(By.name('province')).click();
+    //     // Find the north button
+    //     await driver.findElement(By.name('north')).click();
+    //     // Find the Chiang Rai button
+    //     await driver.findElement(By.name('cnx')).click();
+
+    //     let city = await driver.findElement(By.name('city')).getText();
+    //     // console.log(city);
+    //     assert.equal(city, 'Chiang Rai');
+    // });
     // close the browser after running tests
     after(() => driver && driver.quit());
 });
