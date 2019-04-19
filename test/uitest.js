5fa0af2d0918fd5370ccc554d9e624e7c66e4ddd
Pongsapon A-nong <swu59266@outlook.com>
2019-04-19 21:18:53 +0700

    add uitest

--- test/uitest.js
+++ test/uitest.js
@@ -15,37 +15,89 @@ describe('Checkout Homepage', function () {
         await driver.manage().window().maximize()
         // Load the page
         await driver.get('http://localhost:3000');
-        // Find the menu button
-        // await driver.findElement(By.name('menu')).click();
-        // Find the province button
+        // Find the navigation button
         await driver.findElement(By.name('province')).click();
-        // Find the north button
+        // Find the region button
         await driver.findElement(By.name('north')).click();
-        // Find the Chiang Rai button
+        // Find the province button
         await driver.findElement(By.name('chiang_rai')).click();
 
         let city = await driver.findElement(By.name('city')).getText();
-        // console.log(city);
         assert.equal(city, 'Chiang Rai');
     });
-
-    // it('Click on Chiang Rai', async function () {
-    //     // Set browser size to maximize
-    //     await driver.manage().window().maximize()
-    //     // Load the page
-    //     await driver.get('http://localhost:3000');
-    //     // Find the province button
-    //     await driver.findElement(By.name('province')).click();
-    //     // Find the north button
-    //     await driver.findElement(By.name('north')).click();
-    //     // Find the Chiang Rai button
-    //     await driver.findElement(By.name('cnx')).click();
-
-    //     let city = await driver.findElement(By.name('city')).getText();
-    //     // console.log(city);
-    //     assert.equal(city, 'Chiang Rai');
-    // });
     // close the browser after running tests
     after(() => driver && driver.quit());
 });
+
+// describe('Checkout Homepage', function () {
+//     let driver;
+//     before(async function () {
+//         driver = await new Builder().forBrowser('chrome').build();
+//     });
+//     it('Click on Nan', async function () {
+//         // Set browser size to maximize
+//         await driver.manage().window().maximize()
+//         // Load the page
+//         await driver.get('http://localhost:3000');
+//         // Find the navigation button
+//         await driver.findElement(By.name('province')).click();
+//         // Find the region button
+//         await driver.findElement(By.name('north')).click();
+//         // Find the province button
+//         await driver.findElement(By.name('nan')).click();
+
+//         let city = await driver.findElement(By.name('city')).getText();
+//         assert.equal(city, 'Nan');
+//     });
+//     // close the browser after running tests
+//     after(() => driver && driver.quit());
+// });
+
+// describe('Checkout Homepage', function () {
+//     let driver;
+//     before(async function () {
+//         driver = await new Builder().forBrowser('chrome').build();
+//     });
+//     it('Click on Phrae', async function () {
+//         // Set browser size to maximize
+//         await driver.manage().window().maximize()
+//         // Load the page
+//         await driver.get('http://localhost:3000');
+//         // Find the navigation button
+//         await driver.findElement(By.name('province')).click();
+//         // Find the region button
+//         await driver.findElement(By.name('north')).click();
+//         // Find the province button
+//         await driver.findElement(By.name('phrae')).click();
+
+//         let city = await driver.findElement(By.name('city')).getText();
+//         assert.equal(city, 'Phrae');
+//     });
+//     // close the browser after running tests
+//     after(() => driver && driver.quit());
+// });
+
+// describe('Checkout Homepage', function () {
+//     let driver;
+//     before(async function () {
+//         driver = await new Builder().forBrowser('chrome').build();
+//     });
+//     it('Click on Lamphun', async function () {
+//         // Set browser size to maximize
+//         await driver.manage().window().maximize()
+//         // Load the page
+//         await driver.get('http://localhost:3000');
+//         // Find the navigation button
+//         await driver.findElement(By.name('province')).click();
+//         // Find the region button
+//         await driver.findElement(By.name('north')).click();
+//         // Find the province button
+//         await driver.findElement(By.name('lamphun')).click();
+
+//         let city = await driver.findElement(By.name('city')).getText();
+//         assert.equal(city, 'Lamphun');
+//     });
+//     // close the browser after running tests
+//     after(() => driver && driver.quit());
+// });
 
